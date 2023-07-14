f0d88c4ca6b1231c6349d6a28481ac94de33fb6e
developerruhul <ruhulamin.webdev@gmail.com>
2023-07-14 11:11:12 +0600

    frontend: add dark theme toggle

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@/components/theme-provider';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.className}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </div>
+  );
 }
